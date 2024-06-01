d292be742d10c6bdecb010da03301abe34b74708
Alexei Belozertsev <belozertsev.alexei@gmail.com>
2024-06-01 10:49:36 +0200

    feat: create proposal page

--- src/app/create-proposal/page.tsx
+++ src/app/create-proposal/page.tsx
@@ -1,6 +1,9 @@
-'use client'
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import closeIcon from "../../../public/close-icon.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 type FormValues = {
   title: string;
@@ -8,21 +11,45 @@ type FormValues = {
 };
 
 const CreateProposal: React.FC = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = data => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
   };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const actions = [
+    "Action 1",
+    "Action 2",
+    "Action 3",
+    // Add more actions as needed
+  ];
 
   return (
     <div className="flex flex-col">
       <h1 className="text-3xl font-bold mb-6">Create proposal</h1>
-      <div className="lg:grid lg:grid-cols-2 xs:flex xs:flex-col gap-[24px]">
-        <div className="xs:mb-[24px]">
+      <div className="grid gap-6 lg:grid-cols-2">
+        <div>
           <h2 className="text-2xl font-semibold">Proposal Description</h2>
           <br />
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-4">
-              <label htmlFor="title" className="block text-slate-400 text-sm font-thin">
+              <label
+                htmlFor="title"
+                className="block text-slate-400 text-sm font-thin"
+              >
                 Title
               </label>
               <input
@@ -30,10 +57,15 @@ const CreateProposal: React.FC = () => {
                 {...register("title", { required: true })}
                 className="mt-1 block w-full border border-gray-300 text-slate-900 p-2 rounded-md shadow-sm"
               />
-              {errors.title && <span className="text-red-500">This field is required</span>}
+              {errors.title && (
+                <span className="text-red-500">This field is required</span>
+              )}
             </div>
             <div className="mb-4">
-              <label htmlFor="overview" className="block text-slate-400 text-sm font-thin">
+              <label
+                htmlFor="overview"
+                className="block text-slate-400 text-sm font-thin"
+              >
                 Overview
               </label>
               <textarea
@@ -41,16 +73,58 @@ const CreateProposal: React.FC = () => {
                 {...register("overview", { required: true })}
                 className="mt-1 block w-full min-h-[200px] border border-gray-300 rounded-md shadow-sm p-2 text-slate-900"
               />
-              {errors.overview && <span className="text-red-500">This field is required</span>}
+              {errors.overview && (
+                <span className="text-red-500">This field is required</span>
+              )}
             </div>
-            <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md">
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded-md"
+            >
               Submit
             </button>
           </form>
         </div>
-        <div>
+        <div className="flex flex-col lg:gap-[40px]">
           <h2 className="text-2xl font-semibold">Actions</h2>
-          <br />
+          <button
+            onClick={handleOpenModal}
+            className="mt-2 px-4 py-2 w-full bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-300"
+          >
+            Add action
+          </button>
+          {isModalOpen && (
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+              <div className="bg-white rounded-lg shadow-lg w-3/4 max-h-[400px] overflow-y-auto relative">
+                <div className="flex flex-row justify-between items-center p-4">
+                  <h2 className="text-xl text-slate-500 font-bold">
+                    Add action
+                  </h2>
+                  <button
+                    onClick={handleCloseModal}
+                    className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                  >
+                    <Image
+                      src={closeIcon}
+                      width={18}
+                      height={18}
+                      alt="close icon"
+                    />
+                  </button>
+                </div>
+                <div className="divide-y divide-gray-300 overflow-y-auto">
+                  {actions.map((action, index) => (
+                    <button
+                      key={index}
+                      className="block p-3 w-full text-left text-gray-800 hover:text-blue-600"
+                    >
+                      {action}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
